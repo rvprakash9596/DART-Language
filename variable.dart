7e679bc3ed09253fecea2c104dd84df4c0daa248
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-17 17:36:36 +0530

    variable in dart

---
+++ variable.dart
@@ -0,0 +1,18 @@
+void main()
+{
+    // Declaring and initialising a variable
+    int v1 = 10;
+
+    double v2 = 0.2; // must declare double a value or it will throw error
+
+    bool v3 = false; // must declare boolean a value or it will throw error
+
+    // Declaring multiple variable
+    String v4 = "0", v5 = "ravi prakash 4344";
+
+    print(v1); // Print 10
+    print(v2); // Print 0.2
+    print(v3); // Print default string value
+    print(v4); // Print default bool value
+    print(v5); // Print ravi prakash 4344
+}
