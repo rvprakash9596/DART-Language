92bbfa95ed47fdec4c70081146c83679b78c35a5
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-18 17:53:29 +0530

    Data Types in Dart Programming

--- data_type.dart
+++ data_type.dart
@@ -1,4 +1,5 @@
-void main(){
+void main()
+{
   //1. Number data types
   /*stdout.write("Enter 1st Number :");
   int n1=int.parse(stdin.readLineSync()!);
@@ -21,5 +22,75 @@ void main(){
   // Boolean Data Type
   /*bool v1=true;
   print(v1);*/
+
+  // 4. List Data Type : List data type is similar to arrays in other programming languages. A list is used to represent a collection of objects. It is an ordered group of objects.
+  // There are multiple methods to declare List in Dart as mentioned below:
+  //1. Variable Size List 
+  //2. Fixed Size List
+
+/*
+  List<String> l1= List<String>.filled(3,"default");
+  l1[0]="Saurabh";
+  l1[1]="Caterers";
+  l1[2]="Khalilabad";
+  print(l1); // [Saurabh, Caterers, Khalilabad]
+  print(l1[1]); //Caterers
+*/
+
+// 5. Map DataTypes : The Map object is a key and value pair. Keys and values on a map may be of any type. It is a dynamic collection.
+
+// Declare Map in Dart : While Declaring Map there can be only two cases one where declared Map is empty and another where declared Map contains elements in it.
+
+
+/*
+1. Declaring Empty Map:
+
+// Method 1
+Map? map_name;
+
+// Method 2
+Map<key_datatype , value_datatype>? map_name;
+
+// Method 3
+var map_name = new Map();
+
+2. Declaring Map with Elements inside it.
+
+// Method 1
+Map x={
+     key1 : value1;
+     key2 : value2;
+};
+
+// Method 2
+Map<key_datatype , value_datatype> map_name{
+      key1 : value1;
+      key2 : value2;
+};
+
+// Method 3
+var map_name{
+      key1 : value1;
+      key2 : value2;
+};
+*/
+
+//Example of Map Data Type1.
+/*
+Map ob = new Map();
+ob['First']='Saurabh';
+ob['Second']='Caterers';
+ob['Third']='Khalilabad';
+
+print(ob); // {First: Saurabh, Second: Caterers, Third: Khalilabad}
+*/
+
+/*
+Map obj=new Map();
+obj['Name']="Ravi Prakash";
+obj['Branch']="CSE";
+obj['Roll_No']=2204220109027;
+print(obj); // {Name: Ravi Prakash, Branch: CSE, Roll_No: 2204220109027}
+*/
 
 }
