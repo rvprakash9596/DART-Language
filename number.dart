2d0544ccb3b236bae2fa575ac39adb484be444c3
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-18 15:03:47 +0530

    NUMBER DATATYPES

---
+++ number.dart
@@ -0,0 +1,13 @@
+void main(){
+  int num1=34;
+  double num2=4.25000501;
+  print(num1);
+  print(num2);
+  
+  var a=num.parse("1");
+  var b=num.parse("5.24");
+  
+  var c=a+b;
+  print("Sum = ${c}");
+  
+}
