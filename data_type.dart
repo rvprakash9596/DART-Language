678680df6e272b03517dd70ac24162ed01698bbd
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-18 15:03:19 +0530

    Datatypes

---
+++ data_type.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+void main(){
+  //1. Number data types
+  /*stdout.write("Enter 1st Number :");
+  int n1=int.parse(stdin.readLineSync()!);
+  stdout.write("Enter 2nd Number :");
+  int n2=int.parse(stdin.readLineSync()!);
+  int res=n1+n2;
+  print("The result is= ${res}");*/
+
+  //2.String Data Type
+  /*stdout.write("Enter Youe Name :");
+  String name=stdin.readLineSync()!;
+  print("Your name is ${name}");*/
+
+
+  //3. Double data Type
+  /*stdout.write("Enter your value :");
+  double val=double.parse(stdin.readLineSync()!);
+  print("Value = ${val}");*/
+
+  // Boolean Data Type
+  /*bool v1=true;
+  print(v1);*/
+
+}
