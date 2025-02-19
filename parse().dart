977b0e89fbbdb99cd1baccb1769efbb59d4f29a6
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-19 09:31:04 +0530

    parse() method in dart

---
+++ parse().dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+void main(){
+  /*
+  var n1=num.parse("1");
+  var n2=num.parse("2.39");
+  var res=n1+n2;
+  stdout.write(res);
+  */
+
+/*
+  num a=10;
+  print(a);  //10
+  a=5.025;
+  print(a); //2.025
+*/
+
+/*
+
+*/
+double y=20;
+print(y);
+
+
+}
