2e8e3bd6fd932fc15eb79db44370e89277d97b3e
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-19 09:29:37 +0530

    String in dart

---
+++ String1.dart
@@ -0,0 +1,24 @@
+void main()
+{
+  // A Dart string is a sequence of UTF-16 code units. With the same rule as that of Python, you can use either single or double quotes to create a string. The string starts with the datatype Var : 
+/*
+  var str1="Saurabh Caterers";
+  var str2='$str1 Khalilabad';
+  print(str2);
+  
+  var str3="Hii !";
+  var str4=" Kaise ho";
+  print(str3 + str4);
+
+  */
+
+  var s1="RAVI";
+  var s2="RAVI";
+  if(s1==s2){
+    print('true');
+  }
+  else{
+    print('false');
+  }
+
+}
