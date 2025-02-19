46412add16a737aea7b0ad61a4148319496bf258
rvprakash9596 <raviprakash9596@gmail.com>
2025-02-19 10:33:24 +0530

    Constants in dart

---
+++ Constants.dart
@@ -0,0 +1,24 @@
+void main(){
+  /*
+  In Dart language, Constants are objects whose values cannot be changed during the execution of the program. Hence, they are a type of immutable object. A constant cannot be reassigned any value if a value has been assigned to it earlier. If we try to reassign any value to a constant, Dart throws an exception. 
+
+  In Dart language, we can define constants by using 2 keywords :
+
+  1.final keyword
+  2.const keyword 
+  */
+
+  // int a=20;
+  // a=39;
+  // print(a);
+
+  // const int a=25;
+  // print(a);
+
+  final var1=54;
+  print(var1);
+
+  final String1="Ravi Prakash";
+  print(String1);
+
+}
