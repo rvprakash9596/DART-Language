b113d44e8ac1dab733bcc3718b9eef00c6fe2169
rvprakash9596 <raviprakash9596@gmail.com>
2025-03-05 19:01:22 +0530

    Duplicate element in array set

---
+++ duplicate_ele.dart
@@ -0,0 +1,21 @@
+void findDuplicates(List<int> arr) {
+  Set<int> seen = {};
+  Set<int> duplicates = {};
+
+  for (int num in arr) {
+    if (!seen.add(num)) {
+      duplicates.add(num);
+    }
+  }
+
+  if (duplicates.isEmpty) {
+    print("No duplicates found");
+  } else {
+    print("Duplicate elements: ${duplicates.toList()}");
+  }
+}
+
+void main() {
+  List<int> numbers = [9,8,3,9,0,2,9,5,4,7];
+  findDuplicates(numbers);
+}
