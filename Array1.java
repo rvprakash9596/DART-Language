ecafdfc5d2f06769ff7175efc4ca210188df6eed
rvprakash9596 <raviprakash9596@gmail.com>
2025-03-05 23:16:28 +0530

    Print array element one by one

---
+++ Array1.java
@@ -0,0 +1,9 @@
+public class Array1 {
+    public static void main(String[] args) {
+        int arr[]={10,12,15,14};
+        for(int i=1; i<arr.length; i++)
+        {
+            System.out.println("Element No "+i+" = "+arr[i]);
+        }
+    }
+}
